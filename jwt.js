329cc2448f75498cc52b295aab74765dae2435df
Dilip395 <57963351+Dilip395@users.noreply.github.com>
2020-06-20 19:13:18 +0530

    update funtion work

--- jwt.js
+++ jwt.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+/*
 var algorithm  = 'aes256';
 var key = 'mypassword';
 var name = 'Dilip';
@@ -13,3 +14,23 @@ function myFunction(a, b){
 
 var finalOutput = myFunction(encrypted, decrypted);
 console.log(finalOutput);
+*/
+var algorithm  = 'aes256';
+var salt = 'mypassword';
+
+function myEncrypt(data){
+
+	var cipher = crypto.createCipher(algorithm,salt);
+	var encrypted = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+    return encrypted;
+}
+
+function myDecrypt(encryptString){
+	var decipher = crypto.createDecipher(algorithm,salt);
+	var decrypted = decipher.update(encryptString,'hex', 'utf8') + decipher.final('utf8');
+	return decrypted;
+}
+var  encryptedData = myEncrypt('dilip');
+console.log(encryptedData);
+var  decryptedData = myDecrypt(encryptedData);
+console.log(decryptedData);
